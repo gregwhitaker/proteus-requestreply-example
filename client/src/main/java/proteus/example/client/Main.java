958721b380eed878ab6001b460ab316d78b44744
gregwhitaker <gwhitake@gmail.com>
2018-06-19 10:08:43 -0700

    Doing work

--- client/src/main/java/proteus/example/client/Main.java
+++ client/src/main/java/proteus/example/client/Main.java
@@ -15,9 +15,69 @@
  */
 package proteus.example.client;
 
+import io.netifi.proteus.Proteus;
+import io.netifi.proteus.rsocket.ProteusSocket;
+import proteus.example.service.helloworld.HelloRequest;
+import proteus.example.service.helloworld.HelloWorldServiceClient;
+
 public class Main {
 
     public static void main(String... args) throws Exception {
+        Long accessKey = getAccessKeyFromArgs(args);
+        String accessToken = getAccessTokenFromArgs(args);
 
+        // Define connection to the Proteus Broker
+        final Proteus proteus = Proteus.builder()
+                .group("proteus.example.client")    // Name of this client's group
+                .destination("helloworldclient")    // Name of this client
+                .accessKey(accessKey)               // Access key for connecting to Proteus Broker
+                .accessToken(accessToken)           // Access token for connecting to Proteus Broker
+                .host("localhost")                  // Proteus Broker's host
+                .port(8001)                         // Proteus Broker's port
+                .build();
+
+        // Create virtual connection to the VowelCount service group
+        ProteusSocket conn = proteus.group("proteus.example.service.helloworld");
+
+        // Create hello message to send to service
+        HelloRequest helloRequest = HelloRequest.newBuilder()
+                .setName("World")
+                .build();
+
+        // Send hello message and print the response
+        HelloWorldServiceClient helloClient = new HelloWorldServiceClient(conn);
+        helloClient.sayHello(helloRequest)
+                .doOnNext(helloResponse -> System.out.println(helloResponse.getHelloMessage()));
+
+        // Keep the client running while waiting for asynchronous response
+        Thread.currentThread().join();
+    }
+
+    /**
+     * Gets the access key to use for the demo from the command line arguments.
+     *
+     * @param args command line arguments
+     * @return access key
+     */
+    private static Long getAccessKeyFromArgs(String... args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Invalid number of arguments");
+        }
+
+        return Long.parseLong(args[0]);
+    }
+
+    /**
+     * Gets the access token to use for the demo from the command line arguments.
+     *
+     * @param args command line arguments
+     * @return access token
+     */
+    private static String getAccessTokenFromArgs(String... args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Invalid number of arguments");
+        }
+
+        return args[1];
     }
 }
